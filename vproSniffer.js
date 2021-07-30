8314551102793779f5adff21edcd58e2ec195364
Jack <67034721+lampy255@users.noreply.github.com>
2021-07-30 18:14:59 +1200

    Updates
    - Remove url variable functions to support IE

--- vproSniffer.js
+++ vproSniffer.js
@@ -31,8 +31,8 @@ webServer.options('*', cors()) //CORS Security Option
 webServer.use(express.static('./'));
 webServer.listen(settings.webPort, () => {
     //console.log("WebServer running on port " + settings.webPort);
-    console.log("Open a web browser to http://127.0.0.1:" + settings.webPort + "/web/live.html?signal=1");
-    console.log("Replace the 'signal=1' with whatever signal you want to monitor.");
+    console.log("Open a web browser to http://127.0.0.1:" + settings.webPort + "/web/live-1.html");
+    console.log("Replace the 'live-1' with whatever signal you want to monitor.");
 });
 
 if (settings.devMode) {
@@ -54,69 +54,75 @@ var jpgWeb = {
     }
 };
 
-if (settings.wsOnStart && (settings.frameDiscardInterval > 9)) {
-    console.log("Starting WS..");
-    var socket = new WebSocket(wsUrl);
-    var signal = 0;
-    var recieved = 0;
-    var pictureBuffer = [];
+if (settings.wsOnStart) {
+    if (settings.frameDiscardInterval > 9) {
+        console.log("Starting WS..");
+        var socket = new WebSocket(wsUrl);
+        var signal = 0;
+        var recieved = 0;
+        var pictureBuffer = [];
 
-    socket.onerror = function (event) {
-        console.error("WebSocket error observed:", event);
-    };
+        socket.onerror = function (event) {
+            console.error("WebSocket error observed:", event);
+        };
 
-    socket.onopen = function (event) {
-        console.log("WebSocket is open");
-        if (settings.debugging) {
-            console.log("Sending Subscribe msg...")
-        }
-        socket.send(JSON.stringify(jpgWeb));
-        console.log("Running! Your pip images are in the 'output' folder.");
-        console.log("Capturing every " + settings.frameDiscardInterval + "th frame.");
-    };
-
-    socket.onclose = function (event) {
-        console.log("WebSocket is closed");
-    };
-
-    socket.onmessage = function (event) {
-        recieved++;
-        let payload = event.data.toString('hex');
-
-        let filename;
-
-        if (payload.length < 3) {
-            signal = event.data.charCodeAt(1);
-            //console.log("Signal: " + signal);
-        }
-
-        if (payload.length > 3) {
-            if (typeof signal == 'number') {
-                if (pictureBuffer[signal] == undefined) {
-                    pictureBuffer[signal] = {};
-                }
-                pictureBuffer[signal].data = event.data;
-
-                if (pictureBuffer[signal].count == undefined) {
-                    pictureBuffer[signal].count = 0;
-                } else {
-                    pictureBuffer[signal].count++;
-                }
-
-                let x = (pictureBuffer[signal].count / settings.frameDiscardInterval);
-
-                if (Number.isInteger(x)) {
-                    filename = (settings.dumpDirectory + "Signal-" + signal + ".jpg");
-                    fs.writeFile(filename, pictureBuffer[signal].data, 'base64', (err) => {
-                        if (err) throw err;
-                    });
-                }
-
-
-            }
+        socket.onopen = function (event) {
+            console.log("WebSocket is open");
             if (settings.debugging) {
-                console.log("Received Msg " + recieved);
+                console.log("Sending Subscribe msg...")
+            }
+            socket.send(JSON.stringify(jpgWeb));
+            console.log("Running! Your pip images are in the 'output' folder.");
+            console.log("Capturing every " + settings.frameDiscardInterval + "th frame.");
+        };
+
+        socket.onclose = function (event) {
+            console.log("WebSocket is closed");
+        };
+
+        socket.onmessage = function (event) {
+            recieved++;
+            let payload = event.data.toString('hex');
+
+            let filename;
+
+            if (payload.length < 3) {
+                signal = event.data.charCodeAt(1);
+                //console.log("Signal: " + signal);
+            }
+
+            if (payload.length > 3) {
+                if (typeof signal == 'number') {
+                    if (pictureBuffer[signal] == undefined) {
+                        pictureBuffer[signal] = {};
+                    }
+                    pictureBuffer[signal].data = event.data;
+
+                    if (pictureBuffer[signal].count == undefined) {
+                        pictureBuffer[signal].count = 0;
+                    } else {
+                        pictureBuffer[signal].count++;
+                    }
+
+                    let x = (pictureBuffer[signal].count / settings.frameDiscardInterval);
+
+                    if (Number.isInteger(x)) {
+                        filename = (settings.dumpDirectory + "Signal-" + signal + ".jpg");
+                        fs.writeFile(filename, pictureBuffer[signal].data, 'base64', (err) => {
+                            if (err) throw err;
+                        });
+                    }
+
+
+                }
+                if (settings.debugging) {
+                    console.log("Received Msg " + recieved);
+                }
             }
         }
+    } else {
+        console.log("Error: Frame Discard interval too low.");
     }
+} else {
+    console.log("Not starting WS.");
 }
